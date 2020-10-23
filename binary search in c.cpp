805a50d2b177abd64abdbf1399066869c5b25f70
123saptak <73326885+123saptak@users.noreply.github.com>
2020-10-23 13:19:42 +0530

    Add files via upload

---
+++ binary search in c.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int binary_Search(int arr[], int l, int row, int element)
+{
+ if (row >= l) {
+ int mid = l + (row - l) / 2;
+
+ if (arr[mid] == element)
+ return mid;
+
+
+
+ return binary_Search(arr, mid + 1, row, element);
+ }
+
+
+ return -1;
+}
+
+int main(void)
+{
+ int arr[] = {4,8,23,45,12};
+ int n = sizeof(arr) / sizeof(arr[0]);
+ int element = 10;
+ int find = binary_Search(arr, 0, n - 1, element);
+ (find == -1) ? printf("Element is not present in array") 
+ : printf("Element is present at index %d",
+ find);
+ return 0;
+}
